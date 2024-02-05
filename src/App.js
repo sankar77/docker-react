de571b8340161557c627cca8f3396f8b93ea2f23
sankar77 <nsnkrnryn@gmail.com>
2024-02-05 20:50:57 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Modified content
         </a>
       </header>
     </div>
